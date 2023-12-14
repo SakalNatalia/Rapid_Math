b57737bbf667c0d73db83eec02a36b7b7d53ebf6
MistyFog22 <chowity@gmail.com>
2023-12-14 22:37:23 +0100

    Score Submitted Ablak
    activity_score_submitted.xml

---
+++ app/src/main/java/com/example/rapidmath/SubmitScore.java
@@ -0,0 +1,14 @@
+package com.example.rapidmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_score);
+    }
+}
