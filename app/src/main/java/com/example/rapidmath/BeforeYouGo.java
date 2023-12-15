0964ff51d1648292e2ce5c29981e8047b84ef0d1
MistyFog22 <chowity@gmail.com>
2023-12-15 13:15:33 +0100

    Level Completed Ablak
    activity_level_completed.xml + kezdetleges activity_before_you_go.xml

---
+++ app/src/main/java/com/example/rapidmath/BeforeYouGo.java
@@ -0,0 +1,14 @@
+package com.example.rapidmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeforeYouGo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_before_you_go);
+    }
+}
