6f32436e9e6ea61ec1684bda634d12b3db2c1102
MistyFog22 <chowity@gmail.com>
2023-12-15 11:40:13 +0100

    Level Completed elemek
    Text, Buttons, Rectangles

---
+++ app/src/main/java/com/example/rapidmath/LevelCompleted.java
@@ -0,0 +1,14 @@
+package com.example.rapidmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelCompleted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_completed);
+    }
+}
